d1b8b19afeaf202a019d3103f605da0e68bd9f30
Bhavya Jain <bhavya99044@gmail.com>
2024-11-25 22:29:03 +0530

    first vue commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+  export default{
+    data(){
+      return{
+        message:'hello world',
+        status:'inactive',
+        tasks:['one', 'two', 'three'],
+        link:'http://google.com'
+      }
+    }
+  }
+</script>
+
+<template>
+  <!-- option api -->
+  <!-- /If statement option api -->
+  <h1 v-if="status === 'active'">hello</h1>
+  <h1 v-else-if="status === 'inactive'">bye</h1>
+  <h1 v-else>deactive</h1>
+
+  <!-- for loop -->
+  <ul>
+    <li v-for ="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <!-- link tag -->
+  <a v-bind:href="link" _blank>click</a>
+  <a :href="link" _blank>click</a>
+
+</template>
+
+<style scoped>
+h1{
+  color: red;
+}</style>
