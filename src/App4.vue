785037467c3fdd7b8d6f296576a29ec704c492a8
Bhavya Jain <bhavya99044@gmail.com>
2024-11-30 17:25:51 +0530

    30/11/24

---
+++ src/App4.vue
@@ -0,0 +1,25 @@
+<template>
+<h1>{{ fullName }}</h1>
+</template>
+
+<script>
+
+export default{
+  data(){
+    return {
+      name:'name',
+      lastName:'lastName'
+    }
+  },
+  //for calculate and join the data aftre merge
+  // the need of computed properties is is any value change in the data then method will again and again called but in
+  // but using computed only dependent value chnaged if depended name change
+  computed:{
+    fullName(){
+      return this.name +' '+ this.lastName
+    }
+  }
+
+}
+</script>
+
